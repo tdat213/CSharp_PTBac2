268f1f63942e7a05ab8f42ffa4727f949bbe6bee
SPC <SPC@21-LAB402>
2024-07-17 20:17:34 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Nhap he so a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhap he so b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhap he so c: ");
+        double c = double.Parse(Console.ReadLine());
+
+
+        double delta = b * b - 4 * a * c;
+        if (delta > 0)
+        {
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            Console.WriteLine("Nghiem thu nhat: x1 = {0}", x1);
+            Console.WriteLine("Nghiem thu hai: x2 = {0}", x2);
+        }
+        else if (delta == 0)
+        {
+            double x = -b / (2 * a);
+            Console.WriteLine("Nghiem duy nhat: x = {0}", x);
+        }
+        else
+        {
+            Console.WriteLine("PT vo nghiem");
+        }
+    }
+}
